8b17aa4e7fc8ae554e827433842b28c2f3c3c915
J <joesamban@yahoo.co.uk>
2016-08-05 14:30:56 +0100

    constructor etc.

--- ChessGame/ClassLibrary1/Board.cs
+++ ChessGame/ClassLibrary1/Board.cs
@@ -8,78 +8,83 @@ namespace ClassLibrary1
 {
     public class Board
     {//Each position on the board will have a property and the property will be assigned a value indicating what's in that location.
-        
-        public string a1 { get; set; }
-        public string a2 { get; set; }
-        public string a3 { get; set; }
-        public string a4 { get; set; }
-        public string a5 { get; set; }
-        public string a6 { get; set; }
-        public string a7 { get; set; }
-        public string a8 { get; set; }
 
-        public string b1 { get; set; }
-        public string b2 { get; set; }
-        public string b3 { get; set; }
-        public string b4 { get; set; }
-        public string b5 { get; set; }
-        public string b6 { get; set; }
-        public string b7 { get; set; }
-        public string b8 { get; set; }
+        public Board(Int32 boolStandardSetup)
+        {
 
-        public string c1 { get; set; }
-        public string c2 { get; set; }
-        public string c3 { get; set; }
-        public string c4 { get; set; }
-        public string c5 { get; set; }
-        public string c6 { get; set; }
-        public string c7 { get; set; }
-        public string c8 { get; set; }
+        }
 
-        public string d1 { get; set; }
-        public string d2 { get; set; }
-        public string d3 { get; set; }
-        public string d4 { get; set; }
-        public string d5 { get; set; }
-        public string d6 { get; set; }
-        public string d7 { get; set; }
-        public string d8 { get; set; }
+        public Piece a1 { get; set; }
+        public Piece a2 { get; set; }
+        public Piece a3 { get; set; }
+        public Piece a4 { get; set; }
+        public Piece a5 { get; set; }
+        public Piece a6 { get; set; }
+        public Piece a7 { get; set; }
+        public Piece a8 { get; set; }
 
-        public string e1 { get; set; }
-        public string e2 { get; set; }
-        public string e3 { get; set; }
-        public string e4 { get; set; }
-        public string e5 { get; set; }
-        public string e6 { get; set; }
-        public string e7 { get; set; }
-        public string e8 { get; set; }
+        public Piece b1 { get; set; }
+        public Piece b2 { get; set; }
+        public Piece b3 { get; set; }
+        public Piece b4 { get; set; }
+        public Piece b5 { get; set; }
+        public Piece b6 { get; set; }
+        public Piece b7 { get; set; }
+        public Piece b8 { get; set; }
 
-        public string f1 { get; set; }
-        public string f2 { get; set; }
-        public string f3 { get; set; }
-        public string f4 { get; set; }
-        public string f5 { get; set; }
-        public string f6 { get; set; }
-        public string f7 { get; set; }
-        public string f8 { get; set; }
+        public Piece c1 { get; set; }
+        public Piece c2 { get; set; }
+        public Piece c3 { get; set; }
+        public Piece c4 { get; set; }
+        public Piece c5 { get; set; }
+        public Piece c6 { get; set; }
+        public Piece c7 { get; set; }
+        public Piece c8 { get; set; }
 
-        public string g1 { get; set; }
-        public string g2 { get; set; }
-        public string g3 { get; set; }
-        public string g4 { get; set; }
-        public string g5 { get; set; }
-        public string g6 { get; set; }
-        public string g7 { get; set; }
-        public string g8 { get; set; }
+        public Piece d1 { get; set; }
+        public Piece d2 { get; set; }
+        public Piece d3 { get; set; }
+        public Piece d4 { get; set; }
+        public Piece d5 { get; set; }
+        public Piece d6 { get; set; }
+        public Piece d7 { get; set; }
+        public Piece d8 { get; set; }
 
-        public string h1 { get; set; }
-        public string h2 { get; set; }
-        public string h3 { get; set; }
-        public string h4 { get; set; }
-        public string h5 { get; set; }
-        public string h6 { get; set; }
-        public string h7 { get; set; }
-        public string h8 { get; set; }
+        public Piece e1 { get; set; }
+        public Piece e2 { get; set; }
+        public Piece e3 { get; set; }
+        public Piece e4 { get; set; }
+        public Piece e5 { get; set; }
+        public Piece e6 { get; set; }
+        public Piece e7 { get; set; }
+        public Piece e8 { get; set; }
+
+        public Piece f1 { get; set; }
+        public Piece f2 { get; set; }
+        public Piece f3 { get; set; }
+        public Piece f4 { get; set; }
+        public Piece f5 { get; set; }
+        public Piece f6 { get; set; }
+        public Piece f7 { get; set; }
+        public Piece f8 { get; set; }
+
+        public Piece g1 { get; set; }
+        public Piece g2 { get; set; }
+        public Piece g3 { get; set; }
+        public Piece g4 { get; set; }
+        public Piece g5 { get; set; }
+        public Piece g6 { get; set; }
+        public Piece g7 { get; set; }
+        public Piece g8 { get; set; }
+
+        public Piece h1 { get; set; }
+        public Piece h2 { get; set; }
+        public Piece h3 { get; set; }
+        public Piece h4 { get; set; }
+        public Piece h5 { get; set; }
+        public Piece h6 { get; set; }
+        public Piece h7 { get; set; }
+        public Piece h8 { get; set; }
         
     }
 }
--- ChessGame/ClassLibrary1/Piece.cs
+++ ChessGame/ClassLibrary1/Piece.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ClassLibrary1
 {
-    public interface IPiece
+    public class Piece
     {
         enum name
         {
