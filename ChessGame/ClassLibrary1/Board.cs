f7cf694b10ab5117d778b81ca3377068d500784a
Y2PM <joesamban@yahoo.co.uk>
2016-08-05 10:42:11 +0100

    Made Board class

---
+++ ChessGame/ClassLibrary1/Board.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary1
+{
+    public class Board
+    {//Each position on the board will have a property and the property will be assigned a value indicating what's in that location.
+        public string a1 { get; set; }
+        public string a2 { get; set; }
+        public string a3 { get; set; }
+        public string a4 { get; set; }
+        public string a5 { get; set; }
+        public string a6 { get; set; }
+        public string a7 { get; set; }
+        public string a8 { get; set; }
+
+        public string b1 { get; set; }
+        public string b2 { get; set; }
+        public string b3 { get; set; }
+        public string b4 { get; set; }
+        public string b5 { get; set; }
+        public string b6 { get; set; }
+        public string b7 { get; set; }
+        public string b8 { get; set; }
+
+        public string c1 { get; set; }
+        public string c2 { get; set; }
+        public string c3 { get; set; }
+        public string c4 { get; set; }
+        public string c5 { get; set; }
+        public string c6 { get; set; }
+        public string c7 { get; set; }
+        public string c8 { get; set; }
+
+        public string d1 { get; set; }
+        public string d2 { get; set; }
+        public string d3 { get; set; }
+        public string d4 { get; set; }
+        public string d5 { get; set; }
+        public string d6 { get; set; }
+        public string d7 { get; set; }
+        public string d8 { get; set; }
+
+        public string e1 { get; set; }
+        public string e2 { get; set; }
+        public string e3 { get; set; }
+        public string e4 { get; set; }
+        public string e5 { get; set; }
+        public string e6 { get; set; }
+        public string e7 { get; set; }
+        public string e8 { get; set; }
+
+        public string f1 { get; set; }
+        public string f2 { get; set; }
+        public string f3 { get; set; }
+        public string f4 { get; set; }
+        public string f5 { get; set; }
+        public string f6 { get; set; }
+        public string f7 { get; set; }
+        public string f8 { get; set; }
+
+        public string g1 { get; set; }
+        public string g2 { get; set; }
+        public string g3 { get; set; }
+        public string g4 { get; set; }
+        public string g5 { get; set; }
+        public string g6 { get; set; }
+        public string g7 { get; set; }
+        public string g8 { get; set; }
+
+        public string h1 { get; set; }
+        public string h2 { get; set; }
+        public string h3 { get; set; }
+        public string h4 { get; set; }
+        public string h5 { get; set; }
+        public string h6 { get; set; }
+        public string h7 { get; set; }
+        public string h8 { get; set; }
+        
+    }
+}
