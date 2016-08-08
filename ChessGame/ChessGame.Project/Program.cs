b7b9dfebe2207c76e1434b9274152e53b6dcbcd2
Joseph Samuel Bannert <joesamban@outlook.com>
2016-08-08 11:41:09 +0100

    Moved make positions and print positions method to Board class.

--- ChessGame/ChessGame.Project/Program.cs
+++ ChessGame/ChessGame.Project/Program.cs
@@ -15,49 +15,54 @@ namespace ChessGame.Project
             piece1.colour = Piece.Colour.white;
             piece1.name = Piece.Name.pawn;
 
-
-            string[] rowArray = new string[8] { "1", "2", "3", "4", "5", "6", "7", "8" };//Make positions on a board.
-            string[] colArray = new string[8] { "a", "b", "c", "d", "e", "f", "g", "h" };
-            int numberOfPositions = new int();
-            numberOfPositions = rowArray.Length * colArray.Length;
-            string[] positions = new string[numberOfPositions];
-            int i = new int();
-            i = 0;
-            for (int r = 0; r < rowArray.Length; r++)
-            {
-                for (int c = 0; c < colArray.Length; c++)
-                {
-                    positions[i++] = colArray[c] + rowArray[r];
-                }
-            }
+            Board board = new Board();
+            board.PrintPositions();
 
 
+            #region MyRegion
+            //string[] rowArray = new string[8] { "1", "2", "3", "4", "5", "6", "7", "8" };//Make positions on a board.
+            //string[] colArray = new string[8] { "a", "b", "c", "d", "e", "f", "g", "h" };//create the array.
+            //int numberOfPositions = new int();
+            //numberOfPositions = rowArray.Length * colArray.Length;
+            //string[] positions = new string[numberOfPositions];
+            //int i = new int();
+            //i = 0;//Don't need to set to 0 as it's default value.
+            //for (int r = 0; r < rowArray.Length; r++)
+            //{
+            //    for (int c = 0; c < colArray.Length; c++)
+            //    {
+            //        positions[i++] = colArray[c] + rowArray[r];
+            //    }
+            //}
 
-            int j1 = new int();//Print positions with reversed rows (intuitive layout).
-            for (int j = 7; j > -1; j--)
-            {
-                j1 = 8 * j;
-                for (int k = 0; k < 8; k++)
-                {
-                    Console.Write(positions[j1++] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
 
-            /*
-            int j1 = new int();//Print positions.
-            j1 = 0;
-            for (int j = 0; j < 8; j++)
-            {
-                for (int k = 0; k < 8; k++)
-                {
-                    Console.Write(positions[j1++] + " ");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-            */
+
+            //int j1 = new int();//Print positions with reversed rows (intuitive layout).
+            //for (int j = 7; j > -1; j--)//print the array.
+            //{
+            //    j1 = 8 * j;
+            //    for (int k = 0; k < 8; k++)
+            //    {
+            //        Console.Write(positions[j1++] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+
+            ///*
+            //int j1 = new int();//Print positions.
+            //j1 = 0;
+            //for (int j = 0; j < 8; j++)
+            //{
+            //    for (int k = 0; k < 8; k++)
+            //    {
+            //        Console.Write(positions[j1++] + " ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            //Console.ReadLine();
+            //*/ 
+            #endregion
 
 
         }
--- ChessGame/ClassLibrary1/Board.cs
+++ ChessGame/ClassLibrary1/Board.cs
@@ -9,35 +9,58 @@ namespace ClassLibrary1
     public class Board
     {
 
-
+        string[] positions;
 
         public Board()
         {
-
-
-            #region MyRegion
-
-            string[] rowArray = new string[8] { "1", "2", "3", "4", "5", "6", "7", "8" };
-            string[] colArray = new string[8] { "a", "b", "c", "d", "e", "f", "g", "h" };
+            string[] rowArray = new string[8] { "1", "2", "3", "4", "5", "6", "7", "8" };//Make positions on a board.
+            string[] colArray = new string[8] { "a", "b", "c", "d", "e", "f", "g", "h" };//create the array.
             int numberOfPositions = new int();
             numberOfPositions = rowArray.Length * colArray.Length;
-            string[] positions = new string[numberOfPositions];
+            string[] _positions = new string[numberOfPositions];
             int i = new int();
-            i = 0;
+            i = 0;//Don't need to set to 0 as it's default value.
             for (int r = 0; r < rowArray.Length; r++)
             {
                 for (int c = 0; c < colArray.Length; c++)
                 {
-                    positions[i++] = colArray[c] + rowArray[r];
+                    _positions[i++] = colArray[c] + rowArray[r];
                 }
             }
-
-            #endregion
-
+            positions = _positions;
         }
 
 
 
+        public void PrintPositions()
+        {
+            int j1 = new int();//Print positions with reversed rows (intuitive layout).
+            for (int j = 7; j > -1; j--)//print the array.
+            {
+                j1 = 8 * j;
+                for (int k = 0; k < 8; k++)
+                {
+                    Console.Write(positions[j1++] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+
+        public void PrintPositionsFrom0thElement()
+        {
+            int j1 = new int();//Print positions.
+            j1 = 0;
+            for (int j = 0; j < 8; j++)
+            {
+                for (int k = 0; k < 8; k++)
+                {
+                    Console.Write(positions[j1++] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
 
 
 
